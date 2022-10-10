1b689a16dc0fd04399c4330ad7fc44f2f44896a0
shirley cristina mi <69362121+shirleycristinami@users.noreply.github.com>
2022-10-10 10:52:00 -0300

    Atualização do Sistema em 10/10/2022

--- funcionario/src/test/java/org/iftm/entities/FuncionarioTest.java
+++ funcionario/src/test/java/org/iftm/entities/FuncionarioTest.java
@@ -1,5 +1,19 @@
 package org.iftm.entities;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class FuncionarioTest {
+	public void testarconstrutorComDadosValidos() {
+		
+		@Test
+		public void testarConstrutorAlunoComDadosValidosEAlunoAprovado() {
+			//cenário de teste
+			String nomeValido = "Laiane Angelina Ribeiro";
+			double horasValida = 6.0; //quantidade de horas semanal
+			int valorValida = 75; //valor da hora trabalhada
+			double salarioEsperado = 1800,00;
+			Funcionario a; 
+	}
 	
 }
